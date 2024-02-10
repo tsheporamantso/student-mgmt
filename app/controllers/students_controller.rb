d277c559608fdc1829fdc9fa48804c4b3b82ea88
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-10 07:03:26 +0200

    Add new and create action for student creation

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,4 +7,25 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+
+    if @student.save
+      flash[:success] = 'Student created successfully'
+      redirect_to students_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name, :email)
+  end
 end
